be78c58562c98a1d36101c7789cf1fec4e5536eb
Zahorulko Roman <rr.zagorulko@gmail.com>
2023-09-18 18:45:25 +0300

    Update

--- node-js-new/app.js
+++ node-js-new/app.js
@@ -1,37 +1,57 @@
+const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
 
 const app = express();
 
-app.use(express.static("public"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
-  const filePath = path.join(__dirname, "views", "index.html");
-  res.sendFile(filePath);
+  res.render("index");
 });
 
 app.get("/restaurants", function (req, res) {
-  const filePath = path.join(__dirname, "views", "restaurants.html");
-  res.sendFile(filePath);
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  res.render("restaurants", {
+    numberOfRestaurants: storedRestaurants.length,
+    restaurants: storedRestaurants,
+  });
 });
 
 app.get("/recommend", function (req, res) {
-  const filePath = path.join(__dirname, "views", "recommend.html");
-  res.sendFile(filePath);
+  res.render("recommend");
+});
+
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect("/confirm");
 });
 
 app.get("/confirm", function (req, res) {
-  const filePath = path.join(__dirname, "views", "confirm.html");
-  res.sendFile(filePath);
+  res.render("confirm");
 });
 
 app.get("/about", function (req, res) {
-  const filePath = path.join(__dirname, "views", "about.html");
-  res.sendFile(filePath);
+  res.render("about");
 });
-
-
 
 app.listen(3000);
